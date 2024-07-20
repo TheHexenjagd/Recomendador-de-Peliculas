f1504e98dd29169b7b6cb872e4f58f5110b6cd6e
Gabriel <virtuablade@gmail.com>
2024-07-20 17:48:48 -0500

    v1.0.0 - Base (Archivo)
    Includes files from previous versions

--- app_peliculas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('search/', views.search_movies, name='search_movies'),
-    path('search/random/', views.search_random_movie, name='search_random_movie'),
-]
